955b072ca8f5d0869225cb91ad21dd7763f32390
abhi22-02 <abhishekchechare98.com>
2024-10-26 20:50:29 +0530

    final code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+}
+function closeModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'none';
+}
